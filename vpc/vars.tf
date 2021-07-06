e2d5708146faa361304eb55b22d7a38e8a6ce6e8
sainath <kalvasainathreddy@gmail.com>
2021-07-07 01:21:16 +0530

    vpc creation for dev and prod

--- vpc/vars.tf
+++ vpc/vars.tf
@@ -1,2 +1,6 @@
-variable "VPC_CIDR" {}
-variable "ENV" {}
+variable "VPC_CIDR"               {}
+variable "ENV"                    {}
+variable "PUBLIC_SUBNETS_CIDR"    {}
+variable "PRIVATE_SUBNETS_CIDR"   {}
+variable "SUBNET_ZONES"           {}
+variable "DEFAULT_VPC_ID"         {}
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -6,3 +6,26 @@ resource "aws_vpc" "main" {
     Name = var.ENV
   }
 }
+
+resource "aws_subnet" "public" {
+  count                       = length(var.SUBNET_ZONES)
+  vpc_id                      = aws_vpc.main.id
+  cidr_block                  = element(var.PUBLIC_SUBNETS_CIDR, count.index)
+  availability_zone           = element(var.SUBNET_ZONES, count.index)
+
+  tags = {
+    Name                      = "public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count                       = length(var.SUBNET_ZONES)
+  vpc_id                      = aws_vpc.main.id
+  cidr_block                  = element(var.PRIVATE_SUBNETS_CIDR, count.index)
+  availability_zone           = element(var.SUBNET_ZONES, count.index)
+
+  tags = {
+    Name                      = "private-subnet-${count.index + 1}"
+  }
+}
+
