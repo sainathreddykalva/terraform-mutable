3221fdfceb2fb6428e31d2b498c0743ddf440476
sainath <kalvasainathreddy@gmail.com>
2021-07-07 22:17:50 +0530

    vpc creation for dev and prod

--- vpc/vars.tf
+++ vpc/vars.tf
@@ -1,6 +1,7 @@
-variable "VPC_CIDR"               {}
-variable "ENV"                    {}
-variable "PUBLIC_SUBNETS_CIDR"    {}
-variable "PRIVATE_SUBNETS_CIDR"   {}
-variable "SUBNET_ZONES"           {}
-variable "DEFAULT_VPC_ID"         {}
+variable "VPC_CIDR"                {}
+variable "ENV"                     {}
+variable "PUBLIC_SUBNETS_CIDR"     {}
+variable "PRIVATE_SUBNETS_CIDR"    {}
+variable "SUBNET_ZONES"            {}
+variable "DEFAULT_VPC_ID"          {}
+variable "DEFAULT_VPC_ROUTE_TABLE" {}
