baff96930d2e6e27058462c902092eaa24e79e32
sainath <kalvasainathreddy@gmail.com>
2021-07-11 20:28:28 +0530

    security groups

---
+++ databases/mongodb.tf
@@ -0,0 +1,29 @@
+
+resource "aws_security_group" "allow_mongodb" {
+  name                        = "allow_mongodb"
+  description                 = "AllowMongoDB"
+  vpc_id                      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description               = "SSH"
+    from_port                 = 22
+    to_port                   = 22
+    protocol                  = "tcp"
+    cidr_blocks               = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
+  ingress {
+    description               = "MONGODB"
+    from_port                 = 27017
+    to_port                   = 27017
+    protocol                  = "tcp"
+    cidr_blocks               = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  egress {
+    from_port                 = 0
+    to_port                   = 0
+    protocol                  = "-1"
+    cidr_blocks               = ["0.0.0.0/0"]
+    ipv6_cidr_blocks          = ["::/0"]
+  }
