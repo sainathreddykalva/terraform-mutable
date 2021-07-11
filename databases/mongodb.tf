55dfc05a65bd78b60d736e107db23f27b7a63a1d
sainath <kalvasainathreddy@gmail.com>
2021-07-11 20:35:16 +0530

    security groups

--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -27,3 +27,8 @@ resource "aws_security_group" "allow_mongodb" {
     cidr_blocks               = ["0.0.0.0/0"]
     ipv6_cidr_blocks          = ["::/0"]
   }
+
+  tags                        = {
+    Name                      = "AllowMongoDB"
+  }
+}
