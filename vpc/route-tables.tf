a4955595e456c4b61bec40e34cab77c300fbd884
sainath <kalvasainathreddy@gmail.com>
2021-07-12 21:02:19 +0530

    added wait_for_fulfillment=true for spot instance

--- vpc/route-tables.tf
+++ vpc/route-tables.tf
@@ -47,8 +47,8 @@ resource "aws_route" "private-rt-gateway" {
 
 
 resource "aws_route_table" "public-rt" {
-  vpc_id                          = aws_vpc.main.id
   depends_on                      = [aws_subnet.public, aws_vpc_peering_connection.peer-connection, aws_internet_gateway.igw]
+  vpc_id                          = aws_vpc.main.id
 //  route {
 //    vpc_peering_connection_id     = aws_vpc_peering_connection.peer-connection.id
 //    cidr_block                    = var.DEFAULT_VPC_CIDR
