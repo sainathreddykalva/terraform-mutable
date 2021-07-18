e641b50524a5caf6f3cc8044dc7e23eb918b253a
sainath <kalvasainathreddy@gmail.com>
2021-07-18 09:11:28 +0530

    creatind private and public arn outputs

--- instances/ec2.tf
+++ instances/ec2.tf
@@ -49,7 +49,7 @@ resource "aws_security_group" "allow_ec2" {
   }
 
   tags                        = {
-    Name                      = "Allow${var.COMPONENT}"
+    Name                     = "Allow${var.COMPONENT}"
   }
 }
 
@@ -70,5 +70,19 @@ resource "null_resource" "ansible-apply" {
     ]
 
   }
+}
+
+resource "aws_lb_target_group" "target-group" {
+  name                        = "${var.COMPONENT}-${var.ENV}"
+  port                        = var.PORT
+  protocol                    = "HTTP"
+  vpc_id                      = data.terraform_remote_state.vpc.outputs.VPC_ID
+}
+
+resource "aws_lb_target_group_attachment" "tg-attach" {
+  count                       = var.INSTANCE_COUNT
+  target_group_arn            = aws_lb_target_group.target-group.arn
+  target_id                   = element(aws_spot_instance_request.instances.*.spot_instance_id,count.index)
+  port                        = var.PORT
 }
 
