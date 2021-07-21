5243d3c54419609766f7d1702a563d2ce9920793
sainath <kalvasainathreddy@gmail.com>
2021-07-21 08:43:19 +0530

    update dns record in mogodb.tf

--- databases/rds-mysql.tf
+++ databases/rds-mysql.tf
@@ -55,3 +55,25 @@ resource "aws_security_group" "allow_rds_mysql" {
   }
 }
 
+
+resource "aws_route53_record" "mysql-record" {
+  zone_id                     = data.terraform_remote_state.vpc.outputs.HOSTED_ZONE_ID
+  name                        = "mysql-${var.ENV}.roboshop.internal"
+  type                        = "CNAME"
+  ttl                         = "300"
+  records                     = [aws_db_instance.default.address]
+}
+
+resource "null_resource" "mysql-schema" {
+  provisioner "local-exec" {
+    command =<<EOF
+sudo yum install mysql -y
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql -h ${aws_db_instance.default.address} -u${jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["RDS_MYSQL_USER"]} -p${jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["RDS_MYSQL_PASS"]} <shipping.sql
+EOF
+  }
+}
+
