ada99fe252b7324d280a739442daf168c7c54226
sainath <kalvasainathreddy@gmail.com>
2021-07-21 08:51:53 +0530

    update dns record in mogodb.tf

--- databases/rabbitmq.tf
+++ databases/rabbitmq.tf
@@ -63,7 +63,7 @@ resource "null_resource" "ansible-rabbitmq" {
       "sudo yum install python3-pip -y",
       "sudo pip3 install pip --upgrade",
       "sudo pip3 install ansible==4.1.0",
-      "ansible-pull -i localhost, -U https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps56/_git/ansible roboshop-pull.yml -e COMPONENT=rabbitmq -e ENV=dev"
+      "ansible-pull -i localhost, -U https://github.com/sainathreddykalva/ansible.git roboshop-pull.yml -e COMPONENT=rabbitmq -e ENV=dev"
     ]
 
   }
